c62721f206763ab767248ca6697b0451e3ac4c2e
NeilJinn <jliangyi@gmail.com>
2026-06-16 10:36:59 +0200

    feat(host-phone): add ?host=CODE redirect and /host/:code route

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import JoinPage from "./pages/player/JoinPage";
 import AvatarPage from "./pages/player/AvatarPage";
 import WaitingPage from "./pages/player/WaitingPage";
 import InRoomPage from "./pages/player/InRoomPage";
+import HostPhonePage from "./pages/player/HostPhonePage";
 import FateWerewolfBigScreen from "./pages/games/fate-werewolf/BigScreenPage";
 import FateWerewolfPhone from "./pages/games/fate-werewolf/PhonePage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -22,8 +23,11 @@ function RoomCodeRedirect() {
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const roomCode = params.get("room");
+    const hostCode = params.get("host");
     if (roomCode) {
       navigate(`/join/${roomCode.replace(/\D/g, "")}`, { replace: true });
+    } else if (hostCode) {
+      navigate(`/host/${hostCode.replace(/\D/g, "")}`, { replace: true });
     }
   }, [location.search, navigate]);
   return null;
@@ -48,6 +52,7 @@ function AnimatedRoutes() {
         <Route path="/join/:code" element={<AvatarPage />} />
         <Route path="/play/:code" element={<InRoomPage />} />
         <Route path="/waiting/:code" element={<WaitingPage />} />
+        <Route path="/host/:code" element={<HostPhonePage />} />
 
         {/* Games */}
         <Route path="/game/fate-werewolf/:code" element={<FateWerewolfBigScreen />} />
